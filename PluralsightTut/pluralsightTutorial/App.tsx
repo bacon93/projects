172da61714948580667b22e7fa683e49deac2f25
Jonas Andersson <jonasandersson11@gmail.com>
2019-09-24 19:34:06 +0200

    The core of the tutorial

--- PluralsightTut/pluralsightTutorial/App.tsx
+++ PluralsightTut/pluralsightTutorial/App.tsx
@@ -1,19 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import EventList from './EventList'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>I have now opened App.tsx</Text>
-    </View>
+      <EventList />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
